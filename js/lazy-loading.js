a4537486ef3f0584f55dd17ff7279ddf03eefb73
Ferilax <muhammadfx@mail.ru>
2024-04-05 00:51:57 +0300

    finally (no)

---
+++ js/lazy-loading.js
@@ -0,0 +1,20 @@
+const videos = document.querySelectorAll(".lazy-video");
+
+videos.forEach(el => {
+	const videoId = el.dataset.id;
+	const playButton = el.querySelector(".play-button");
+
+	el.style.backgroundImage = `url(https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg)`;
+
+	playButton.addEventListener("click", () => {
+		el.innerHTML = `
+			<iframe
+				src="https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe>
+		`;
+
+		el.classList.add("ready");
+	})
+})
